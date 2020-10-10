5d0a4ec97221c4c19da4b19cc8264b3805588f77
yovel <yovel98@gmail.com>
2020-10-10 16:12:24 +0300

    Added check and delete icons for each todo

--- src/Components/ToDoList/ToDo/ToDo.component.js
+++ src/Components/ToDoList/ToDo/ToDo.component.js
@@ -1,13 +1,25 @@
 import React from "react"
 
 import "./ToDo.css";
+import Check from "../../../Icons/tick-mark.svg";
+import Delete from "../../../Icons/delete.svg";
 
 const ToDo = ({ todo, key }) => {
     return (
         <li key={key} id="list-item">
-            <button id="check-todo">Check</button>
+            <img src={Check} 
+                 id="check-image" 
+                 alt="Check" 
+                 onClick={() => alert("check-to-do")}>
+
+            </img>
             {todo.title}
-            <button id="delete-todo">Delete</button>
+            <img src={Delete} 
+                 id="delete-image" 
+                 alt="Delete"
+                 onClick={() => alert("delete-to-do")}>
+
+            </img>
         </li>
     )
 }
