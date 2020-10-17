5b912c79360b46ea67dfc5f88cbd56b6d59347e1
yovel <yovel98@gmail.com>
2020-10-17 21:23:53 +0300

    Added handling for new todo

--- src/Components/ToDoList/ToDo/ToDo.component.js
+++ src/Components/ToDoList/ToDo/ToDo.component.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React, {useState} from "react"
 
 import "./ToDo.css";
 import Check from "../../../Icons/tick-mark.svg";
 import Delete from "../../../Icons/delete.svg";
 
 const ToDo = ({ todo, index, onCheck, onDelete, onSetTodoValue }) => {
-    console.log(index);
+    const [inputValue, setInputValue] = useState("");
     return (
         <li key={index} id="list-item">
             <img src={Check} 
@@ -19,8 +19,15 @@ const ToDo = ({ todo, index, onCheck, onDelete, onSetTodoValue }) => {
                 <span className={todo.isFinished ? "finished-todo" : ""}>{todo.title}</span>
                 :
                 <div id="todo-input">
-                    <input id="todo-text-input" type="text" placeholder="What do you need to do?"></input>
-                    <button id="todo-input-save">Save!</button>
+                    <input id="todo-text-input" 
+                           type="text" 
+                           value={inputValue}
+                           onChange={(e) => setInputValue(e.target.value)}
+                           placeholder="What do you need to do?"></input>
+                    <button id="todo-input-save" 
+                            onClick={() => onSetTodoValue(index, inputValue)}
+                    > Save!
+                    </button>
                 </div>
             }
             
